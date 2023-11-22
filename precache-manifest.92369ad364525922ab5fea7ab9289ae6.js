6c265e2eba7ba2249273eabf527a545d722fdbaa
priyanshi-srivastava <priyanshi.srivastava@borderfree.io>
2023-11-22 23:22:58 +0530

    Updates

---
+++ precache-manifest.92369ad364525922ab5fea7ab9289ae6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a62a9964beb99915d5b",
+    "url": "/Ten-Pin-Bowling/static/css/main.3234d0f9.chunk.css"
+  },
+  {
+    "revision": "0a62a9964beb99915d5b",
+    "url": "/Ten-Pin-Bowling/static/js/main.0a62a996.chunk.js"
+  },
+  {
+    "revision": "74dbc16ba1813a7f9c18",
+    "url": "/Ten-Pin-Bowling/static/js/1.74dbc16b.chunk.js"
+  },
+  {
+    "revision": "5795decd070beb364f8d",
+    "url": "/Ten-Pin-Bowling/static/js/runtime~main.5795decd.js"
+  },
+  {
+    "revision": "bbbd52c6b29f9f182fe4567d0af2559e",
+    "url": "/Ten-Pin-Bowling/index.html"
+  }
+];
